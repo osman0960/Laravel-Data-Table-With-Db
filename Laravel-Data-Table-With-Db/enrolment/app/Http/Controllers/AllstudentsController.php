729b5953ed9c1e398fd8ecd3fcba9f35d2a59b07
osman0960 <osmanforhad@haotmail.com>
2018-08-27 12:46:17 +0600

    Laravel Insert data with image and Dislpay inage from Db Using Data Table

---
+++ Laravel-Data-Table-With-Db/enrolment/app/Http/Controllers/AllstudentsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class AllstudentsController extends Controller
+{
+    public function allstudent() {
+        
+        $allstudent_info=DB::table('student_tbl')
+                ->get();
+        $manage_student=view('admin.allstudent')
+                ->with('allstudent_info',$allstudent_info);
+        return view('layout')
+        ->with('allstudent',$manage_student);
+        
+        //return view('admin.allstudent');
+    }
+}
